5e6536bddb11fa7865f2e824ebb9a5d92600b2db
BETH WACEKE KIMANI <kbwaceke@gmail.com>
2025-05-10 20:04:33 +0300

    completed the first labs in zybook

---
+++ module_1/1.23LAB:Divide_by_X.py
@@ -0,0 +1,12 @@
+'''Type your code here.'''
+# Create variables for two user inputs
+user_num = int(input())
+x = int(input())
+
+# Divide user_num by x three times
+result1 = user_num // x
+result2 = result1 // x
+result3 = result2 // x
+
+# Output the results
+print(result1, result2, result3)
